20099f2dbd8507919cf0faebd9cc634cfb337199
soueu1991 <95188396+soueu1991@users.noreply.github.com>
2024-11-29 12:19:48 +0000

    load balancer

--- main.tf
+++ main.tf
@@ -45,6 +45,35 @@ subnet_id = module.blog_vpc.public_subnets[0]
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = "i-0f6d38a07d50d080f"
+    }
+  }
+
+  http_tcp_listners = [{
+    port = 80
+    protocol = "HTTP"
+    target_group_index = 0
+  }]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
 module "blog_sg"{
   source = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
