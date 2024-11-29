83e00e811b5a625b9e3134497bcc277bef5cf462
soueu1991 <95188396+soueu1991@users.noreply.github.com>
2024-11-29 11:43:51 +0000

    fix https

--- main.tf
+++ main.tf
@@ -38,8 +38,8 @@ resource "aws_security_group" "blog"{
 
 resource "aws_security_group_rule" "blog_https_in"{
   type = "ingress"
-  from_port = 80
-  to_port = 80
+  from_port = 443
+  to_port = 443
   protocol = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
