4e16e7e3ff5947b3416b178a7f438330cad39d66
soueu1991 <95188396+soueu1991@users.noreply.github.com>
2024-11-29 11:39:25 +0000

    fix

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_security_group_rule" "blog_http_in"{
   protocol = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  aws_security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_everything_out"{
@@ -53,5 +53,5 @@ resource "aws_security_group_rule" "blog_everything_out"{
   protocol = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-  aws_security_group_id = aws_security_group.blog.id
+ security_group_id = aws_security_group.blog.id
 }
